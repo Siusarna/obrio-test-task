e8ca65d10aaabe689e90f36f9ae738bbeb3ff471
Dmytro Petruniak <dmytro.petruniak@igama.ua>
2024-08-26 23:43:23 +0300

    Run lint:fix

--- src/common/middlewares/request-log.middleware.ts
+++ src/common/middlewares/request-log.middleware.ts
@@ -1,83 +1,66 @@
-import { type FastifyInstance, type FastifyReply, type FastifyRequest } from 'fastify';
-import { Logger } from "@nestjs/common";
+import {
+  type FastifyInstance,
+  type FastifyReply,
+  type FastifyRequest,
+} from 'fastify';
+import { Logger } from '@nestjs/common';
 
 export function applyRequestLogger(
-	fastifyInstance: FastifyInstance,
-	appLogger: Logger,
+  fastifyInstance: FastifyInstance,
+  appLogger: Logger,
 ) {
-	const requestLogFn = requestLogMiddleware(appLogger);
+  const requestLogFn = requestLogMiddleware(appLogger);
 
-	fastifyInstance.addHook('onResponse', (request, reply, done) => {
-		requestLogFn(request, reply);
-		done();
-	});
+  fastifyInstance.addHook('onResponse', (request, reply, done) => {
+    requestLogFn(request, reply);
+    done();
+  });
 }
-function requestLogMiddleware(
-	logger: Logger,
-) {
-	return function (req: FastifyRequest, res: FastifyReply) {
-		let duration: number | string = 'unknown';
-
-		logger.log(formatLogMessage(req, res, duration), {
-			httpRequest: {
-				remoteIp: req.ip,
-				method: req.method,
-				duration,
-				url: req.originalUrl,
-				statusCode: res.statusCode,
-				request: formatRequestLog(req),
-			},
-		});
-	};
+function requestLogMiddleware(logger: Logger) {
+  return function (req: FastifyRequest, res: FastifyReply) {
+    logger.log({
+      remoteIp: req.ip,
+      method: req.method,
+      url: req.originalUrl,
+      statusCode: res.statusCode,
+      request: formatRequestLog(req),
+    });
+  };
 }
 
-function formatLogMessage(
-	req: FastifyRequest,
-	res: FastifyReply,
-	duration: number | string,
-) {
-	return `[${req.method}] ${req.originalUrl} - return ${res.statusCode} in ${duration} ms`;
+function formatRequestLog(req: FastifyRequest) {
+  return {
+    body: formatRequestBodyToLog(req),
+    query: formatRequestQueryToLog(req),
+  };
 }
 
-function formatRequestLog(
-	req: FastifyRequest,
-) {
-	return {
-		body: formatRequestBodyToLog(req),
-		query: formatRequestQueryToLog(req),
-	};
+function formatRequestBodyToLog(req: FastifyRequest): string {
+  try {
+    const contentType = (req.headers['content-type'] as string)?.toLowerCase();
+
+    if (!contentType?.includes('application/json')) {
+      return '[is not a json]';
+    }
+
+    const bodyObj = {
+      ...(req.body as Record<string, any>),
+    };
+
+    return JSON.stringify(bodyObj);
+  } catch (e) {
+    return `Error during parsing body: ${e}`;
+  }
 }
 
-function formatRequestBodyToLog(
-	req: FastifyRequest,
-): string {
-	try {
-		const contentType = (req.headers['content-type'] as string)?.toLowerCase();
+function formatRequestQueryToLog(req: FastifyRequest): string {
+  try {
+    const queryObj = {
+      ...(req.query as Record<string, any>),
+    };
 
-		if (!contentType?.includes('application/json')) {
-			return '[is not a json]';
-		}
-
-		let bodyObj = {
-			...(req.body as Record<string, any>),
-		};
-
-		return JSON.stringify(bodyObj);
-	} catch (e) {
-		return `Error during parsing body: ${e}`;
-	}
-}
-
-function formatRequestQueryToLog(
-	req: FastifyRequest,
-): string {
-	try {
-		let queryObj = {
-			...(req.query as Record<string, any>),
-		};
-
-		return JSON.stringify(queryObj);
-	} catch (e) {
-		return `Error during parsing query: ${e}`;
-	}
+    return JSON.stringify(queryObj);
+  } catch (e) {
+    return `Error during parsing query: ${e}`;
+  }
 }
--- src/common/middlewares/response.interceptor.ts
+++ src/common/middlewares/response.interceptor.ts
@@ -1,42 +1,42 @@
 import {
-	type CallHandler,
-	type ExecutionContext,
-	Injectable,
-	type NestInterceptor,
+  type CallHandler,
+  type ExecutionContext,
+  Injectable,
+  type NestInterceptor,
 } from '@nestjs/common';
 import { map, type Observable } from 'rxjs';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
-	constructor() {}
+  constructor() {}
 
-	intercept(
-		context: ExecutionContext,
-		next: CallHandler,
-	): Observable<any> | Promise<Observable<any>> {
-		return next.handle().pipe(
-			map(async responseBody => {
-				const data = await responseBody;
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map(async (responseBody) => {
+        const data = await responseBody;
 
-				if (!data) {
-					return {
-						status: 1,
-						data: {}
-					}
-				}
+        if (!data) {
+          return {
+            status: 1,
+            data: {},
+          };
+        }
 
-				if (!data.status && data.error) {
-					return {
-						status: 0,
-						error: data.error,
-					};
-				}
+        if (!data.status && data.error) {
+          return {
+            status: 0,
+            error: data.error,
+          };
+        }
 
-				return {
-					status: 1,
-					data,
-				};
-			}),
-		);
-	}
+        return {
+          status: 1,
+          data,
+        };
+      }),
+    );
+  }
 }
